4adf6266ee3df6e7d9f2b2fb23a53769aebceae3
edard3v <edard3v@gmail.com>
2025-01-13 18:43:21 -0500

    verificando q envien prefix Bearer

---
+++ src/errors/Bearer.err.ts
@@ -0,0 +1,11 @@
+import { HTTPException } from "hono/http-exception";
+
+export class BearerErr extends HTTPException {
+  constructor() {
+    super(400, {
+      message:
+        "El encabezado de autorización debe incluir un token Bearer válido",
+    });
+    this.name = "BearerErr";
+  }
+}
--- src/middlewares/verifyAuth.middleware.ts
+++ src/middlewares/verifyAuth.middleware.ts
@@ -2,12 +2,14 @@ import type { MiddlewareHandler } from "hono";
 import { JWT } from "../services/tokens/jwt.ts";
 import type { JwtPayload } from "jsonwebtoken";
 import { Unauthorized } from "errors/Unauthorized.err.ts";
+import { BearerErr } from "errors/Bearer.err.ts";
 
 export const verifyAuth: MiddlewareHandler<T> = async (context, next) => {
   const Authorization = context.req.header("Authorization");
   if (!Authorization) throw new Unauthorized();
 
-  const token = Authorization.split(" ").at(1);
+  const [prefix, token] = Authorization.split(" ");
+  if (prefix !== "Bearer") throw new BearerErr();
   if (!token) throw new Unauthorized();
 
   const tokenPayload = JWT.verify(token) as JwtPayload;
